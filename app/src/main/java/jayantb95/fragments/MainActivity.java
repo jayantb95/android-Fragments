4a4d27e9a5ac4886c4b074b22f574cef3b037530
Jayant <jayantb95@live.com>
2018-01-08 02:37:52 +0530

    initial commit.

---
+++ app/src/main/java/jayantb95/fragments/MainActivity.java
@@ -0,0 +1,13 @@
+package jayantb95.fragments;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
